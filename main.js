fa43be0aaf75637cac879449ab5b83879dfe3d4f
jhonatanbsouza <135228782+jhonatanbsouza@users.noreply.github.com>
2024-06-06 08:11:43 -0300

    06/06

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 const botoes = document.querySelectorAll(".botao");
-console.log(botoes);
+const conteudo = document.querySelectorAll(".aba-conteudo")
 for(let i = 0; i < botoes.length ; i++){
     botoes[i].onclick = function(){
         for (let j = 0 ; j < botoes.length ; j++){
             botoes[j].classList.remove("ativo");
+            conteudo[j].classList.remove("ativo")
         }
         botoes[i].classList.add("ativo")
+        conteudo[i].classList.add("ativo")
     }
 }
+
+// Foi colocado display none na classe aba-conteudo no CSS
+// Foram adicionadas as linhas 2, 7 e 10 deste arquivo
+// Essas linhas servem para apagar a classe ativo de todos
+// os conteúdos e adicionar a classe ativo somente
+// no conteúdo relacionado com o botão que foi clicado.
