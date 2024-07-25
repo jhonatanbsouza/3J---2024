9a7cd6b4f1a3845a7be0b3ab290d7256c8323b6f
jhonatanbsouza <135228782+jhonatanbsouza@users.noreply.github.com>
2024-07-25 08:11:32 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,24 +1,40 @@
 const botoes = document.querySelectorAll(".botao");
 const conteudo = document.querySelectorAll(".aba-conteudo");
 const tempo = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date(2025,10,20);
-const agora = new Date();
-
-let segundos = Math.floor((tempoObjetivo1-agora)/1000)
-let minutos = Math.floor(segundos/60)
-let horas = Math.floor(minutos/60)
-let dias = Math.floor(horas/24)
-
-segundos = segundos%60
-minutos = minutos%60
-horas = horas%24
-
-tempo[0].textContent = `Faltam ${dias} dias, ${horas} horas, ${minutos} minutos e ${segundos} segundos`
-
-
-
 // guarda as divs contador na variável tempo
-console.log(tempo);
+const tempoObjetivo1 = new Date(2023,10,25,18,0)
+const tempoObjetivo2 = new Date(2024,8,25,18,0)
+const tempoObjetivo3 = new Date(2024,10,25,18,0)
+const tempoObjetivo4 = new Date(2024,10,25,18,0)
+
+function calculaTempo(tempoObjetivo){
+    const agora = new Date()
+    let segundos
+    let minutos
+    let horas
+    let dias
+    segundos = Math.floor((tempoObjetivo-agora)/1000)
+    minutos = Math.floor(segundos/60)
+    horas = Math.floor(minutos/60)
+    dias = Math.floor(horas/24)
+
+    segundos = segundos%60
+    minutos = minutos%60
+    horas = horas%24
+    // Se o meu tempo for maior que 0,
+    // Senao retorn 'Prazo Finalizado'
+    if (segundos >= 0){
+        return `Faltam ${dias} dias, ${horas} horas, ${minutos} minutos e ${segundos} segundos`
+    } else {
+        return 'Prazo Finalizado!'
+    }    
+}
+
+tempo[0].textContent = calculaTempo(tempoObjetivo1);
+tempo[1].textContent = calculaTempo(tempoObjetivo2);
+tempo[2].textContent = calculaTempo(tempoObjetivo3);
+tempo[3].textContent = calculaTempo(tempoObjetivo4)
+
 for(let i = 0; i < botoes.length ; i++){
     botoes[i].onclick = function(){
         for (let j = 0 ; j < botoes.length ; j++){
